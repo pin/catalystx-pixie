f84fbae713dffd131dd9a3bd1b34aa06abf5493f
Dmitri Popov <pin@pin-laptop.(none)>
2010-01-15 02:14:09 +0300

    just from module-starter

---
+++ lib/CatalystX/Pixie.pm
@@ -0,0 +1,107 @@
+package CatalystX::Pixie;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+CatalystX::Pixie - The great new CatalystX::Pixie!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use CatalystX::Pixie;
+
+    my $foo = CatalystX::Pixie->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dmitri Popov, C<< <operator at cv.dp-net.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-catalystx-pixie at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CatalystX-Pixie>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc CatalystX::Pixie
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CatalystX-Pixie>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/CatalystX-Pixie>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/CatalystX-Pixie>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/CatalystX-Pixie>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Dmitri Popov, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of CatalystX::Pixie
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'CatalystX::Pixie' );
+}
+
+diag( "Testing CatalystX::Pixie $CatalystX::Pixie::VERSION, Perl $], $^X" );
